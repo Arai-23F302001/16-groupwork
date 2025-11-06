32d0d734715ea8f3a90f7159a88a14b8920ab008
Arai-23F302001 <rgod11511@gmail.com>
2025-11-06 14:14:10 +0900

    ファイルパス変更

--- app/src/pages/controllers/AuthPage.tsx
+++ app/src/pages/controllers/AuthPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SectionCard } from "../components/Ui";
+import { SectionCard } from "../../components/Ui";
 
 export default function AuthPage({
   onLogin,
